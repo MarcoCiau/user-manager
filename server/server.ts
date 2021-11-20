db10953e276315cf3f13d1870d01ba93a7b9ac61
Marco A. Ciau <marco@makerlab.mx>
2021-11-20 14:03:36 -0600

    cors, urlencoded & json middlewares setup

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,25 @@
 import express, { Application } from 'express';
 import envConfig from '../config/config';
+import cors from 'cors';
 
 class Server {
     private app: Application;
     private port: string;
-
+    private corsOrigin: string;
     constructor() {
         this.app = express();
         this.port = envConfig.SERVER_PORT || '8000';
+        this.corsOrigin = envConfig.CORS_ORIGIN || 'localhost:8000';
+        this.middlewares();
+    }
+
+    middlewares() {
+        /* enable cors */
+        this.app.use(cors({origin: this.corsOrigin}));
+        /* parse application/json requests*/
+        this.app.use(express.json);
+        /* parse application/x-www-form-urlencoded requests. Only parse string or arrays*/
+        this.app.use(express.urlencoded({extended: false}));
     }
 
     listen() {
