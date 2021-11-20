324ba88f9c12462d51859ba7b7a5950e1b919db5
Marco A. Ciau <marco@makerlab.mx>
2021-11-20 14:02:14 -0600

    cors setup

--- config/config.ts
+++ config/config.ts
@@ -2,5 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 export default {
     NODE_ENV: process.env.ENV,
-    SERVER_PORT : process.env.PORT
+    SERVER_PORT : process.env.PORT,
+    CORS_ORIGIN: process.env.CORS_ORIGIN,
 };
