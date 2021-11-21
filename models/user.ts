7ea6ed3aacd458465e31592ae14043207d1c4d60
Marco A. Ciau <marco@makerlab.mx>
2021-11-21 11:53:45 -0600

    fix model : add password field & remove name field

--- models/user.ts
+++ models/user.ts
@@ -1,13 +1,13 @@
 import { Schema, model } from "mongoose";
 
 interface User {
-    name: string,
-    email: string
+    email: string,
+    password : string, 
 }
 
 const userSchema = new Schema<User>({
-    name: { type: String, required: true },
-    email: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 });
 
 const UserModel = model<User>('User', userSchema);
