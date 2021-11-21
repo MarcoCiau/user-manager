582171d3dec578785e22a417e3fe6177070ac053
Marco A. Ciau <marco@makerlab.mx>
2021-11-21 12:27:17 -0600

    auth validator middlewares validate email & password

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as authController from '../controllers/auth';
+import * as authValidator from '../middlewares/auth.validator';
 const router = Router();
 
-router.post('/signup', authController.signup);
+router.post('/signup', authValidator.rules(), authValidator.result, authController.signup);
 router.post('/signin', authController.signin);
 router.post('/forgot', authController.forgotPassword);
 router.post('/reset', authController.resetPassword);
