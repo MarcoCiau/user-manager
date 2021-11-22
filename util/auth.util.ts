e1f62f270c1e7139d9b3e306c1c1620050f6417b
Marco A. Ciau <marco@makerlab.mx>
2021-11-21 20:08:34 -0600

    generate jwt token util

--- util/auth.util.ts
+++ util/auth.util.ts
@@ -5,6 +5,8 @@ generate JWT
 validate JWT
 */
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import envConfig from '../config/config';
 
 export const hashPassword = async (password: string) => {
     try {
@@ -14,4 +16,18 @@ export const hashPassword = async (password: string) => {
         console.log('Hashing password failed : ', error);
         return '';
     }
+}
+
+export const generateJWT = async (userId: string) => {
+    return new Promise<string>((resolve, reject) => {
+        jwt.sign({ userId }, envConfig.JWT_PRIVATE_KEY, { expiresIn: '1h' }, function (error, token = '') {
+            if (error) {
+                reject(error);
+            }
+            if (!token) {
+                reject('Invalid JWT Token.')
+            }
+            resolve(token);
+        });
+    })
 }
