bb23574cd17f7abc5366b9699b628a0ff4416bae
Marco A. Ciau <marco@makerlab.mx>
2021-11-21 18:23:24 -0600

    bcrypt salt rounds as env variable

--- config/config.ts
+++ config/config.ts
@@ -4,5 +4,6 @@ export default {
     NODE_ENV: process.env.ENV,
     SERVER_PORT : process.env.PORT,
     CORS_ORIGIN: process.env.CORS_ORIGIN,
-    DB_URL: process.env.MONGODB_SRV 
+    DB_URL: process.env.MONGODB_SRV,
+    SALT_ROUNDS: process.env.SALT_ROUNDS
 };
