c2b5a1e795009c8161d016e6f56ae18147f9ee3a
Marco A. Ciau <marco@makerlab.mx>
2021-11-21 21:01:29 -0600

    implement signin controller

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -16,13 +16,26 @@ export const signup = async (req: Request, res: Response) => {
         const token: string = await generateJWT(result._id);
         res.status(200).json({ msg: 'success', user: result, token });
     } catch (error) {
-        console.log('Signing up user failed.');
+        console.log('Signing up user failed.', error);
         res.status(500).json({ msg: 'something went wrong.' })
     }
 }
 
-export const signin = (req: Request, res: Response) => {
-    res.status(200).json({ msg: 'signin' });
+export const signin = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+        const userExists = await UserModel.findOne({ email });
+        if (!userExists) return res.status(400).json({ msg: 'User Doesn\'t exists or the email is invalid.' });
+        const isValidPassword = await userExists.comparePassword(password);
+        if (!isValidPassword) {
+            return res.status(400).json({ msg: 'Invalid Password.' });
+        }
+        const token: string = await generateJWT(userExists._id);
+        res.status(200).json({ msg: 'success', user: userExists, token });
+    } catch (error) {
+        console.log('Signing in user failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
 }
 
 export const forgotPassword = (req: Request, res: Response) => {
