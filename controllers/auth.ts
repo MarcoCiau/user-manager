574f7fdc83ae808672a92a33ce5e6f3c42afe205
Marco A. Ciau <marcoantoniociau@hotmail.com>
2021-11-26 00:44:46 -0600

    forgot password logic implemented todo: send email

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import UserModel from '../models/user';
+import TokenModel from '../models/token';
 import { hashPassword, generateJWT } from '../util/auth.util';
+import crypto from 'crypto';
 
 export const signup = async (req: Request, res: Response) => {
     const { email, password } = req.body;
@@ -38,8 +40,30 @@ export const signin = async (req: Request, res: Response) => {
     }
 }
 
-export const forgotPassword = (req: Request, res: Response) => {
-    res.status(200).json({ msg: 'forgot password' });
+export const forgotPassword = async (req: Request, res: Response) => {
+    /*
+    - check if user exists
+    - check if there is an available token, if yes, delete it
+    - create new tolen with crypto
+    - hash token and save it to db
+    - send email with template & url that contain an userId & token (plain text)
+    */
+    const { email} = req.body;
+    const user = await UserModel.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'User Doesn\'t exists or the email is invalid.' });
+    const oldToken = await TokenModel.findOne({userId: user._id});
+    if (oldToken) oldToken.deleteOne();
+    let newToken:string = crypto.randomBytes(32).toString('hex');
+    let hashedToken:string = await hashPassword(newToken);
+    const tokenDoc = new TokenModel({
+        userId: user._id,
+        token: hashedToken, 
+        createdAt: new Date()
+    });
+    const result = await tokenDoc.save();
+    if (!result) return res.status(500).json({ msg: 'sGenerating new token failed' });
+    //TODO: send email
+    res.status(200).json({ msg: 'forgot password', result });
 }
 
 export const resetPassword = (req: Request, res: Response) => {
