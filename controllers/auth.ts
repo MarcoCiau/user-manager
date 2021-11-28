b38fbc338f2785f57611c09f77db821a75cc511d
Marco A. Ciau <marcoantoniociau@hotmail.com>
2021-11-28 17:24:21 -0600

    refresh token controller added TODO: test it

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import config from '../config/config';
 import UserModel from '../models/user';
 import TokenModel from '../models/token';
-import { hashPassword, compareHash, generateJWT, sendEmail } from '../util/auth.util';
+import { hashPassword, compareHash, generateAccessToken, generateRefreshToken, verifyRefreshToken, sendEmail } from '../util/auth.util';
 import crypto from 'crypto';
 
 export const signup = async (req: Request, res: Response) => {
@@ -11,13 +11,17 @@ export const signup = async (req: Request, res: Response) => {
         const userExists = await UserModel.findOne({ email });
         if (userExists) return res.status(400).json({ msg: 'User already exists.' });
         const hashedPassword: string = await hashPassword(password);
+
         const userDoc = new UserModel({
             email,
             password: hashedPassword
         });
+        
         const result = await userDoc.save();
-        const token: string = await generateJWT(result._id);
-        res.status(200).json({ msg: 'success', user: result, token });
+        const [refreshToken, accessToken] = await Promise.all([generateRefreshToken(result._id), generateAccessToken(result._id)]);
+        const refreshTokenEncrypted = await hashPassword(refreshToken);
+        const updatedUser = await UserModel.findOneAndUpdate({ _id: result._id }, { refreshToken: refreshTokenEncrypted }, { new: true });
+        res.status(200).json({ msg: 'success', user: updatedUser, accessToken, refreshToken });
     } catch (error) {
         console.log('Signing up user failed.', error);
         res.status(500).json({ msg: 'something went wrong.' })
@@ -33,8 +37,10 @@ export const signin = async (req: Request, res: Response) => {
         if (!isValidPassword) {
             return res.status(400).json({ msg: 'Invalid Password.' });
         }
-        const token: string = await generateJWT(userExists._id);
-        res.status(200).json({ msg: 'success', user: userExists, token });
+        const [refreshToken, accessToken] = await Promise.all([generateRefreshToken(userExists._id), generateAccessToken(userExists._id)]);
+        const refreshTokenEncrypted = await hashPassword(refreshToken);
+        const updatedUser = await UserModel.findOneAndUpdate({ _id: userExists._id }, { refreshToken: refreshTokenEncrypted }, { new: true });
+        res.status(200).json({ msg: 'success', user: updatedUser, accessToken, refreshToken });
     } catch (error) {
         console.log('Signing in user failed.', error);
         res.status(500).json({ msg: 'something went wrong.' })
@@ -92,5 +98,30 @@ export const resetPassword = async (req: Request, res: Response) => {
     } catch (error) {
         console.log('reset user password failed.', error);
         res.status(500).json({ msg: 'something went wrong.' })
+    }
+}
+
+export const refreshToken = async (req: Request, res: Response) => {
+    /*
+    - check if token exists
+    - compare plain token with hashed
+    - verify access token : todo
+    - create new access & refresh token
+    */
+    try {
+        const { refreshToken, userId } = req.body;
+        const user = await UserModel.findOne({userId});
+        if (!user) return res.status(400).json({ msg: 'User doesn\'t exists or the userId is Invalid.' });
+        const isValidToken: boolean = await compareHash(refreshToken, user.refreshToken);
+        if (!isValidToken) return res.status(400).json({ msg: 'Invalid refresh token.' });
+        const validRefreshToken = await verifyRefreshToken(refreshToken);
+        if (!validRefreshToken) return res.status(400).json({ msg: 'Invalid refresh token.' })
+        const [newRefreshToken, accessToken] = await Promise.all([generateRefreshToken(user._id), generateAccessToken(user._id)]);
+        const refreshTokenEncrypted = await hashPassword(newRefreshToken);
+        const updatedUser = await UserModel.findOneAndUpdate({ _id: user._id }, { refreshToken: refreshTokenEncrypted }, { new: true });
+        res.status(200).json({ msg: 'success', user: updatedUser, accessToken, refreshToken: newRefreshToken });
+    } catch (error) {
+        console.log('reset user password failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' });
     }
 }
