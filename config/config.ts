ceab54f8a8f97730b085365942ec356b1154c5b2
Marco A. Ciau <marcoantoniociau@hotmail.com>
2021-11-28 13:27:10 -0600

    reset password client url

--- config/config.ts
+++ config/config.ts
@@ -5,5 +5,6 @@ export default {
     SERVER_PORT : process.env.PORT,
     CORS_ORIGIN: process.env.CORS_ORIGIN,
     DB_URL: process.env.MONGODB_SRV,
-    JWT_PRIVATE_KEY: process.env.JWT_KEY || '1234567890ABCDEFGHIJK'
+    JWT_PRIVATE_KEY: process.env.JWT_KEY || '1234567890ABCDEFGHIJK',
+    CLIENT_URL: process.env.CLIENT_URL || 'localhost'
 };
