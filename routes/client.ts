4d71c285cf0a2338c4ea9364a84a125481668db8
Marco A. Ciau <marcoantoniociau@hotmail.com>
2022-01-10 11:36:04 -0600

    using routes

--- routes/client.ts
+++ routes/client.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as clientController from "../controllers/client";
+import * as clientValidator from "../middlewares/client.validator";
 const router = Router();
 
 router.get('/', clientController.getClients);
-router.get('/:id', clientController.getClient);
-router.post('/', clientController.createClient);
-router.put('/:id', clientController.updateClient);
-router.delete('/:id', clientController.deleteClient);
+router.get('/:id', clientValidator.isMongoId(), clientValidator.checkForErrors, clientController.getClient);
+router.post('/', clientValidator.validateBodyParams(), clientValidator.checkForErrors, clientController.createClient);
+router.put('/:id', clientValidator.isMongoId(), clientValidator.validateBodyParams(), clientValidator.checkForErrors, clientController.updateClient);
+router.delete('/:id', clientValidator.isMongoId(), clientValidator.checkForErrors, clientController.deleteClient);
 
 export default router;
