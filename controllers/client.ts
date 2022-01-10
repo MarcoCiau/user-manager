104d314aca5a62e828b000b6c2dc9220b763cddf
Marco A. Ciau <marcoantoniociau@hotmail.com>
2022-01-10 11:35:58 -0600

    controller implementation

--- controllers/client.ts
+++ controllers/client.ts
@@ -1,27 +1,75 @@
-import {Request, Response } from 'express';
+import { Request, Response } from 'express';
 import ClientModel from '../models/client';
 
-export const getClients = (req: Request, res: Response) => {
-    res.status(200).json({ msg: 'get clients' });
+export const getClients = async (req: Request, res: Response) => {
+    try {
+        const result = await ClientModel.find();
+        res.status(200).json({ msg: 'success', clients: result });
+    } catch (error) {
+        console.log('Get all Clients user failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
 }
 
-export const getClient = (req: Request, res: Response) => {
-    const { id } = req.params;
-    res.status(200).json({ msg: 'get client', id });
+export const getClient = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const result = await ClientModel.findOne({ _id: id });
+        if (!result) {
+            return res.status(400).json({ msg: 'Client doesn\'t exists' });
+        }
+        res.status(200).json({ msg: 'success', client: result });
+    } catch (error) {
+        console.log('Get client failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
 }
 
-export const createClient= (req: Request, res: Response) => {
-    const { body } = req.body;
-    res.status(200).json({ msg: 'create client', body });
+export const createClient = async (req: Request, res: Response) => {
+
+    try {
+        const { email } = req.body;
+        const clientExists = await ClientModel.findOne({ email });
+        if (clientExists) return res.status(400).json({ msg: 'Client already exists.' });
+        const clientDocument = new ClientModel({
+            ...req.body
+        });
+        const result = await clientDocument.save();
+        res.status(200).json({ msg: 'success', client: result });
+    } catch (error) {
+        console.log('Create client failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
 }
 
-export const updateClient = (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { body } = req.body;
-    res.status(200).json({ msg: 'update client', id, body });
+export const updateClient = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const body = req.body;
+        console.log(id, body);
+
+        const result = await ClientModel.findOneAndUpdate({ _id: id }, { ...body }, { new: true });
+        if (!result) {
+            return res.status(400).json({ msg: 'Client doesn\'t exists' });
+        }
+        res.status(200).json({ msg: 'success', client: result });
+    } catch (error) {
+        console.log('update client failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' });
+    }
 }
 
-export const deleteClient = (req: Request, res: Response) => {
-    const { id } = req.params;
-    res.status(200).json({ msg: 'delete client', id });
+export const deleteClient = async (req: Request, res: Response) => {
+
+    try {
+        const { id } = req.params;
+        const result = await ClientModel.findByIdAndDelete({ _id: id });
+        if (!result) {
+            return res.status(400).json({ msg: 'Client doesn\'t exists' });
+        }
+        res.status(200).json({ msg: 'success', client: result });
+    } catch (error) {
+        console.log('Delete client failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' });
+    }
 }
